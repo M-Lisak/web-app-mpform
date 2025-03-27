bc9618737089cbab49a9a44d595815b6e925d72d
nazzar123 <139446554+nazzar123@users.noreply.github.com>
2025-03-27 16:50:39 +0300

    add login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { useTelegram } from './hooks/useTelegram.js'
 
 function App() {
   const { tg } = useTelegram()
-  // const [ login, setLogin ] = useState('')
-  // const [ password, setPassword ] = useState('')
+  const [ login, setLogin ] = useState('')
+  const [ password, setPassword ] = useState('')
 
   useEffect(() => {
     tg.ready()
@@ -14,12 +14,27 @@ function App() {
     // tg.enableClosingConfirmation()
   }, [tg])
 
-  const signIn = () => {
-    console.log('sign in')
+  const signIn = async () => {
+    //отправить запрос на бэк, если вернётся success, значит всё збс
+    console.log('sign in', login, password)
+    
   }
   
   return (
     <div className="App">
+      <input
+          onChange={e => setLogin(e.target.value)}
+          value={login}
+          type="text"
+          placeholder="Номер телефона/почта"
+      />
+      <input
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          placeholder="Пароль"
+      />
+      {<span>errors?</span>}
       <button onClick={signIn}>Войти</button>
     </div>
   )
